ba88f007ace73c7e5ccafa68ead25ec7b2c95a8f
Sndav <bossstyle@126.com>
2016-03-31 20:20:30 +0800

    zoomeye login

--- auto_exp/lib/core/finder/zoomeye/login.py
+++ auto_exp/lib/core/finder/zoomeye/login.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+import requests
+
+class Zoo_login(object):
+	def __init__(self,user,passd):
+		self.dic = {
+			"username":user,
+			"password":passd
+		}
+		self.dic = json.dumps(self.dic)
+	def get_token(self):
+		res = requests.post("http://api.zoomeye.org/user/login",data = self.dic)
+		res = res.text
+		json_t = json.loads(res)
+		return json_t
+
 if __name__ == "__main__":
 	pass
