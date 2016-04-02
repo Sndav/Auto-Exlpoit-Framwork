c9d77e52e0f4818c6b8e9a3ce1ee6b3e91ac21cb
Sndav <bossstyle@126.com>
2016-04-02 11:35:16 +0800

    netjson

--- auto_exp/lib/core/network/netjson.py
+++ auto_exp/lib/core/network/netjson.py
@@ -5,12 +5,25 @@ import json
 import requests
 
 class N_json(object):
-	def request(self,url,dic,header = ""):
-		j = json.dumps(dic)
-		res = requests.post(url,j,header=dic(header))
-		res = res.text
-		j = json.loads(res)
-		return j
+	def request(self, url, dic=None, headers=None,method = "post"):
+		if method == "post":
+			if dic is None:
+				dic = {}
+			if headers is None:
+				headers = {}
+			j = json.dumps(dic)
+			res = requests.post(url,j,headers=headers)
+			print res.text
+			res = res.json()
+			return res
+		else :
+			if method == "get":
+				res = requests.get(url,headers=headers)
+				print res.text
+				res = res.json()
+				return res
+
 
 if __name__ == "__main__":
 	pass
+
