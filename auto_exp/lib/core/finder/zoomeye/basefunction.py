fb5c27149182090a4fdf0c158acecf0adab517bd
Sndav <bossstyle@126.com>
2016-04-02 11:35:05 +0800

    zoomeye

--- auto_exp/lib/core/finder/zoomeye/basefunction.py
+++ auto_exp/lib/core/finder/zoomeye/basefunction.py
@@ -4,7 +4,6 @@
 from auto_exp.lib.core.output.output import O_Output
 from auto_exp.lib.core.network.netjson import N_json
 from auto_exp.lib.core.finder.zoomeye.login import Zoo_login
-import json
 
 class Zoo_function(object):
 	output = O_Output()
@@ -31,5 +30,6 @@ class Zoo_function(object):
 		url = "https://api.zoomeye.org/web/search\?query\=\""+query+"\"&page="+str(page)+"&facet="+facets
 		res = self.njson.request(url,headers=headers,method="get")
 		return  res
+
 if __name__ == "__main__":
 	pass
--- auto_exp/lib/core/finder/zoomeye/main.py
+++ auto_exp/lib/core/finder/zoomeye/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-if __name__ == "__main__":
+from auto_exp.lib.core.finder.zoomeye.basefunction import Zoo_function
+
+class Zoo_main(Zoo_function):
 	pass
