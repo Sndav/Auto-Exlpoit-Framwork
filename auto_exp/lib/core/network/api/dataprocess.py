21be1eafd72c8815cb9720799a65705941c6fa58
Sndav <bossstyle@126.com>
2016-06-06 21:07:49 +0800

    v0.0.3 add some api

---
+++ auto_exp/lib/core/network/api/dataprocess.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class A_data(object):
+	def dtc(self):
+		pass
+
+if __name__ == "__main__":
+	pass
+
---
+++ auto_exp/lib/core/network/api/listener.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class A_listener(object):
+	pass
+
+if __name__ == "__main__":
+	pass
+
